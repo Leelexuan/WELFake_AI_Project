198c6e39552c1fc646cd9045f2389c5f7123fff0
Leelexuan <leelxuan@gmail.com>
2025-04-01 17:27:47 +0800

    feat: added functions for stylo features

--- feat_eng/feat_eng.py
+++ feat_eng/feat_eng.py
@@ -9,3 +9,30 @@ age sentence length in words”, “frequency of words of
  Fig. 3, it is observed that real news has a very high aver
 age number of quotes compared to fake news.
 '''
+
+import pandas as pd
+import re
+import string
+
+
+
+def has_quotes(text):
+    return bool(re.search(r'".+"', text))
+
+def has_url(text):
+    regex = r"[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
+    return bool(re.search(regex, text))
+
+def percent_uppercase(text):
+    return sum(1 for c in text if c.isupper())/ len(text) * 100 if len(text) > 0 else 0
+
+def frequency_punctuation(text):
+    punctuation_set = set(string.punctuation)
+    return sum(1 for char in text if char in punctuation_set)
+
+def percent_whitespace(text):
+    return text.count(' ')/ len(text) * 100 if len(text) > 0 else 0
+
+def frequency_words_15(text):
+    return
+
