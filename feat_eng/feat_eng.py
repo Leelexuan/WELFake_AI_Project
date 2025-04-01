549811c76e565e255aab3d914a525ed51a551105
Leelexuan <leelxuan@gmail.com>
2025-04-01 16:06:06 +0800

    chore: restructured repo

---
+++ feat_eng/feat_eng.py
@@ -0,0 +1,11 @@
+'''
+In feature set 3 (Table 2), the ten features
+ with highest importance values are “has quoted content”,
+ “has URL”, “% of uppercase letters”, “frequency of punc
+tuation”, “frequency of words of length 15”, “% of
+ whitespaces”, “frequency of words of length 14”, “aver
+age sentence length in words”, “frequency of words of
+ length 12” and “frequency of words of length 11”. In
+ Fig. 3, it is observed that real news has a very high aver
+age number of quotes compared to fake news.
+'''
